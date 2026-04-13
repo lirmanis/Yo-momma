51cff08f820309a1face98785291cc478b8c963e
lirmanis <ip25.k.lirmanis@vtdt.edu.lv>
2026-04-13 11:25:29 +0300

    Update insults.js

--- insults.js
+++ insults.js
@@ -1,52 +1,55 @@
 let x = 0;
 let jk = 0;
-
-let container = document.getElementById("balz");
-container.innerHTML = "Select category and amount of jokes";
+let z=0;
 
 async function fetchJokes() {
   let container2 = document.getElementById("balz2");
-  let kate = document.getElementById("kate").value;
   container2.innerHTML = ""
 
   try {
-    async function ReJoke() {
-      if (jk >= x) {
-        x = 0;
-        jk = 0;
-        return;
-      }
-  
-      fetch(`https://yomama-jokes.com/api/random`)
-        .then(response => response.json())
+    async function joker() {
+        if (jk >= x) {
+            x = 0;
+            jk = 0;
+            return;
+        }
 
-        .then(data => {
-            if(data.joke.includes(kate)) {
-                container2.innerHTML += `~ ${data.joke} <br><br>`;
-                jk++;
-                ReJoke();
-            }
-            else {
-              ReJoke()
-            }
-        });
+    const response = await fetch(`https://yomama-jokes.com/api/random`)
+
+    if(!response.ok) {
+      throw new Error("error")
     }
-    ReJoke()
+        const data = await response.json()
+        container2.innerHTML += `~ ${data.joke} <br><br>`;
+        jk++;
+        joker()
+    }
+    joker()
   }
   catch(error) {
-    console.error(error)
+    console.log(error)
     return
   }
 }
 
 async function listQ() {
+
+    if(z>=7) {
+      document.getElementById('wrapper').style.display = 'block';
+      return
+    }
+
   let e = document.getElementById("dropdown").value;
   if (e > 3) {
     return alert("Max jokes 3 vro"); 
   }
   else if (e > 0) {
     x = e;
+    z=z+e;
   }
 
   fetchJokes();
+}
+function closebtn(){
+  document.getElementById('wrapper').style.display = 'none';
 }
